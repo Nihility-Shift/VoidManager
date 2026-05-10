e8bdb1075e226da93ba5a4db12e151de7b06cde7
Nagord <46509577+Nagord@users.noreply.github.com>
2026-05-09 17:15:58 -0700

    Moddified Supported content loader to Detect and find BepInEx paths location through Reflection.

--- VoidManager/Content/SupportedContentLoader.cs
+++ VoidManager/Content/SupportedContentLoader.cs
@@ -1,6 +1,7 @@
 ﻿using RuntimeAssets;
 using System;
 using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace VoidManager.Content
@@ -10,9 +11,24 @@ namespace VoidManager.Content
     /// </summary>
     public class SupportedContentLoader
     {
+        static Type BepinType;
+        static MethodInfo PluginsPathMI;
+
         internal static void Initialize()
         {
-            foreach (string fileName in Directory.EnumerateFiles(BepInEx.Paths.PluginPath, "*.metem", SearchOption.AllDirectories))
+            BepinType = Type.GetType("BepInEx.Paths, BepInEx");
+            if (BepinType == null)
+            {
+                Debug.Log("Type null");
+                return;
+            }
+            PluginsPathMI = BepinType.GetMethod("get_PluginPath", BindingFlags.Static | BindingFlags.Public | BindingFlags.GetProperty);
+            if (PluginsPathMI == null)
+            {
+                Debug.Log("PathMethod null");
+                return;
+            }
+            foreach (string fileName in Directory.EnumerateFiles((string)PluginsPathMI.Invoke(null, null), "*.metem", SearchOption.AllDirectories))
             {
                 try
                 {
